0ba7d7b30d0ffd363c5050552a4a201bb18c762c
PadelRivas <yayoscantera@gmail.com>
2025-08-26 18:03:47 +0000

    Actualización de backend.js desde Colab

--- backend.js
+++ backend.js
@@ -98,6 +98,11 @@ const appData = {
             "id_resultado": 1,
             "id_partido": 1,
             "equipo_ganador": 1
+        },
+        {
+            "id_resultado": 2,
+            "id_partido": 2,
+            "equipo_ganador": 1
         }
     ],
     "sets": [
@@ -114,6 +119,27 @@ const appData = {
             "numero_set": 2,
             "juegos_equipo1": 6,
             "juegos_equipo2": 4
+        },
+        {
+            "id_set": 3,
+            "id_partido": 2,
+            "numero_set": 1,
+            "juegos_equipo1": 6,
+            "juegos_equipo2": 1
+        },
+        {
+            "id_set": 4,
+            "id_partido": 2,
+            "numero_set": 2,
+            "juegos_equipo1": 3,
+            "juegos_equipo2": 6
+        },
+        {
+            "id_set": 5,
+            "id_partido": 2,
+            "numero_set": 3,
+            "juegos_equipo1": 7,
+            "juegos_equipo2": 6
         }
     ],
     "partido_pareja": [
