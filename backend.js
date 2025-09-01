b99b7cf88aab0a31b35cb62d78acd517bbeeb121
Trixe311 <58938167+Trixe311@users.noreply.github.com>
2025-09-01 14:20:13 +0200

    TestDataFinal

--- backend.js
+++ backend.js
@@ -1,46 +1,62 @@
 const appData = {
-    "jugadores": [
+"jugadores": [
         {
             "id_jugador": 1,
-            "nombre": "Adrian"
+            "nombre": "CHAMPI"
         },
         {
             "id_jugador": 2,
-            "nombre": "Sergio"
+            "nombre": "DAVID"
         },
         {
             "id_jugador": 3,
-            "nombre": "David"
+            "nombre": "MELERO"
         },
         {
             "id_jugador": 4,
-            "nombre": "Javi"
+            "nombre": "NACHO"
         },
         {
             "id_jugador": 5,
-            "nombre": "Pablo"
+            "nombre": "JAVI"
         },
         {
             "id_jugador": 6,
-            "nombre": "Carlos"
+            "nombre": "TOMICO"
+        },
+        {
+            "id_jugador": 7,
+            "nombre": "GONZALO"
+        },
+        {
+            "id_jugador": 8,
+            "nombre": "SERGIO"
         }
     ],
     "partidos": [
         {
             "id_partido": 1,
-            "fecha": "2025-09-01"
+            "fecha": "2025-08-28"
         },
         {
             "id_partido": 2,
-            "fecha": "2025-09-08"
+            "fecha": "2025-08-28"
         },
         {
             "id_partido": 3,
-            "fecha": "2025-09-15"
+            "fecha": "2025-08-28"
         },
         {
             "id_partido": 4,
-            "fecha": "2025-09-22"
+            "fecha": "2025-08-28"
+        },
+        {
+            "id_partido": 5,
+            "fecha": "2025-08-28"
+        },
+        {
+            "id_partido": 6,
+            "fecha": "2025-08-28"
         }
     ],
     "parejas": [
@@ -56,193 +72,232 @@ const appData = {
         },
         {
             "id_pareja": 3,
-            "id_jugador1": 5,
-            "id_jugador2": 6
-        },
-        {
-            "id_pareja": 4,
             "id_jugador1": 1,
             "id_jugador2": 3
         },
         {
-            "id_pareja": 5,
-            "id_jugador1": 2,
-            "id_jugador2": 5
-        },
-        {
-            "id_pareja": 6,
-            "id_jugador1": 4,
+            "id_pareja": 4,
+            "id_jugador1": 5,
             "id_jugador2": 6
         },
         {
-            "id_pareja": 7,
+            "id_pareja": 5,
             "id_jugador1": 1,
             "id_jugador2": 4
         },
         {
-            "id_pareja": 8,
+            "id_pareja": 6,
             "id_jugador1": 2,
-            "id_jugador2": 3
+            "id_jugador2": 5
+        },
+        {
+            "id_pareja": 7,
+            "id_jugador1": 1,
+            "id_jugador2": 7
+        },
+        {
+            "id_pareja": 8,
+            "id_jugador1": 6,
+            "id_jugador2": 8
         },
         {
             "id_pareja": 9,
-            "id_jugador1": 5,
-            "id_jugador2": 6
+            "id_jugador1": 1,
+            "id_jugador2": 8
+        },
+        {
+            "id_pareja": 10,
+            "id_jugador1": 2,
+            "id_jugador2": 4
+        },
+        {
+            "id_pareja": 11,
+            "id_jugador1": 4,
+            "id_jugador2": 8
         }
     ],
     "resultados": [
-        // Partido 1: Adrian y Sergio ganan a David y Javi
         {
             "id_resultado": 1,
             "id_partido": 1,
             "equipo_ganador": 1
         },
-        // Partido 2: Adrian y David ganan a Sergio y Pablo
         {
             "id_resultado": 2,
             "id_partido": 2,
-            "equipo_ganador": 4
+            "equipo_ganador": 1
         },
-        // Partido 3: Javi y Carlos ganan a Adrian y Javi
         {
             "id_resultado": 3,
             "id_partido": 3,
-            "equipo_ganador": 6
+            "equipo_ganador": -1
         },
-        // Partido 4: Se produce un empate. Los 4 jugadores de este partido tendrán los mismos puntos
         {
             "id_resultado": 4,
             "id_partido": 4,
-            "equipo_ganador": -1
+            "equipo_ganador": 1
+        },
+        {
+            "id_resultado": 5,
+            "id_partido": 5,
+            "equipo_ganador": 0
+        },
+        {
+            "id_resultado": 6,
+            "id_partido": 6,
+            "equipo_ganador": 1
         }
     ],
     "sets": [
-        // Sets del Partido 1
         {
             "id_set": 1,
             "id_partido": 1,
             "numero_set": 1,
             "juegos_equipo1": 6,
-            "juegos_equipo2": 4
+            "juegos_equipo2": 0
         },
         {
             "id_set": 2,
             "id_partido": 1,
             "numero_set": 2,
             "juegos_equipo1": 6,
-            "juegos_equipo2": 3
+            "juegos_equipo2": 4
         },
-        // Sets del Partido 2
         {
             "id_set": 3,
             "id_partido": 2,
             "numero_set": 1,
             "juegos_equipo1": 6,
-            "juegos_equipo2": 2
+            "juegos_equipo2": 1
         },
         {
             "id_set": 4,
             "id_partido": 2,
             "numero_set": 2,
-            "juegos_equipo1": 6,
-            "juegos_equipo2": 4
+            "juegos_equipo1": 3,
+            "juegos_equipo2": 6
         },
-        // Sets del Partido 3
         {
             "id_set": 5,
-            "id_partido": 3,
-            "numero_set": 1,
-            "juegos_equipo1": 4,
-            "juegos_equipo2": 6
-        },
-        {
-            "id_set": 6,
-            "id_partido": 3,
-            "numero_set": 2,
-            "juegos_equipo1": 2,
-            "juegos_equipo2": 6
-        },
-        // Sets del Partido 4
-        {
-            "id_set": 7,
-            "id_partido": 4,
-            "numero_set": 1,
-            "juegos_equipo1": 6,
-            "juegos_equipo2": 4
-        },
-        {
-            "id_set": 8,
-            "id_partido": 4,
-            "numero_set": 2,
-            "juegos_equipo1": 4,
-            "juegos_equipo2": 6
-        },
-        {
-            "id_set": 9,
-            "id_partido": 4,
+            "id_partido": 2,
             "numero_set": 3,
             "juegos_equipo1": 7,
             "juegos_equipo2": 6
         },
         {
-            "id_set": 10,
+            "id_set": 6,
             "id_partido": 4,
-            "numero_set": 4,
+            "numero_set": 1,
             "juegos_equipo1": 6,
-            "juegos_equipo2": 7
+            "juegos_equipo2": 1
+        },
+        {
+            "id_set": 7,
+            "id_partido": 4,
+            "numero_set": 2,
+            "juegos_equipo1": 6,
+            "juegos_equipo2": 1
+        },
+        {
+            "id_set": 8,
+            "id_partido": 5,
+            "numero_set": 1,
+            "juegos_equipo1": 6,
+            "juegos_equipo2": 4
+        },
+        {
+            "id_set": 9,
+            "id_partido": 5,
+            "numero_set": 2,
+            "juegos_equipo1": 5,
+            "juegos_equipo2": 2
+        },
+        {
+            "id_set": 10,
+            "id_partido": 6,
+            "numero_set": 1,
+            "juegos_equipo1": 6,
+            "juegos_equipo2": 3
         },
         {
             "id_set": 11,
-            "id_partido": 4,
-            "numero_set": 5,
+            "id_partido": 6,
+            "numero_set": 2,
             "juegos_equipo1": 6,
-            "juegos_equipo2": 6
+            "juegos_equipo2": 3
         }
     ],
     "partido_pareja": [
-        // Partido 1
         {
+            "id_partido_pareja": 1,
             "id_partido": 1,
             "id_pareja": 1,
             "equipo": 1
         },
         {
+            "id_partido_pareja": 2,
             "id_partido": 1,
             "id_pareja": 2,
             "equipo": 2
         },
-        // Partido 2
         {
+            "id_partido_pareja": 3,
+            "id_partido": 2,
+            "id_pareja": 3,
+            "equipo": 1
+        },
+        {
+            "id_partido_pareja": 4,
             "id_partido": 2,
             "id_pareja": 4,
-            "equipo": 1
-        },
-        {
-            "id_partido": 2,
-            "id_pareja": 5,
             "equipo": 2
         },
-        // Partido 3
         {
+            "id_partido_pareja": 5,
             "id_partido": 3,
-            "id_pareja": 7,
+            "id_pareja": 5,
             "equipo": 1
         },
         {
+            "id_partido_pareja": 6,
             "id_partido": 3,
             "id_pareja": 6,
             "equipo": 2
         },
-        // Partido 4
         {
+            "id_partido_pareja": 7,
             "id_partido": 4,
-            "id_pareja": 8,
+            "id_pareja": 7,
             "equipo": 1
         },
         {
+            "id_partido_pareja": 8,
             "id_partido": 4,
+            "id_pareja": 8,
+            "equipo": 2
+        },
+        {
+            "id_partido_pareja": 9,
+            "id_partido": 5,
             "id_pareja": 9,
+            "equipo": 1
+        },
+        {
+            "id_partido_pareja": 10,
+            "id_partido": 5,
+            "id_pareja": 10,
+            "equipo": 2
+        },
+        {
+            "id_partido_pareja": 11,
+            "id_partido": 6,
+            "id_pareja": 1,
+            "equipo": 1
+        },
+        {
+            "id_partido_pareja": 12,
+            "id_partido": 6,
+            "id_pareja": 11,
             "equipo": 2
         }
-    ]
-};
+    ]};
