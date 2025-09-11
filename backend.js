dcb33cb47b46fa0a3a3b54ec560094531a9fe9bc
PadelRivas <yayoscantera@gmail.com>
2025-09-11 19:04:12 +0000

    Actualización de backend.js desde Colab

--- backend.js
+++ backend.js
@@ -15,12 +15,28 @@ const appData = {
         {
             "id_jugador": 4,
             "nombre": "OSCAR"
+        },
+        {
+            "id_jugador": 5,
+            "nombre": "DAVID"
+        },
+        {
+            "id_jugador": 6,
+            "nombre": "MELERO"
+        },
+        {
+            "id_jugador": 7,
+            "nombre": "NACHO"
         }
     ],
     "partidos": [
         {
             "id_partido": 1,
             "fecha": "2025-09-04"
+        },
+        {
+            "id_partido": 2,
+            "fecha": "2025-09-11"
         }
     ],
     "parejas": [
@@ -33,12 +49,27 @@ const appData = {
             "id_pareja": 2,
             "id_jugador1": 3,
             "id_jugador2": 4
+        },
+        {
+            "id_pareja": 3,
+            "id_jugador1": 1,
+            "id_jugador2": 5
+        },
+        {
+            "id_pareja": 4,
+            "id_jugador1": 6,
+            "id_jugador2": 7
         }
     ],
     "resultados": [
         {
             "id_resultado": 1,
             "id_partido": 1,
+            "equipo_ganador": 1
+        },
+        {
+            "id_resultado": 2,
+            "id_partido": 2,
             "equipo_ganador": 1
         }
     ],
@@ -56,6 +87,20 @@ const appData = {
             "numero_set": 2,
             "juegos_equipo1": 6,
             "juegos_equipo2": 1
+        },
+        {
+            "id_set": 3,
+            "id_partido": 2,
+            "numero_set": 1,
+            "juegos_equipo1": 6,
+            "juegos_equipo2": 2
+        },
+        {
+            "id_set": 4,
+            "id_partido": 2,
+            "numero_set": 2,
+            "juegos_equipo1": 6,
+            "juegos_equipo2": 3
         }
     ],
     "partido_pareja": [
@@ -69,6 +114,18 @@ const appData = {
             "id_partido_pareja": 2,
             "id_partido": 1,
             "id_pareja": 2,
+            "equipo": 2
+        },
+        {
+            "id_partido_pareja": 3,
+            "id_partido": 2,
+            "id_pareja": 3,
+            "equipo": 1
+        },
+        {
+            "id_partido_pareja": 4,
+            "id_partido": 2,
+            "id_pareja": 4,
             "equipo": 2
         }
     ]};
