fd9c3f359576d3f7ea07a81d2b39d8c70c2bf3d5
PadelRivas <yayoscantera@gmail.com>
2026-04-09 18:48:23 +0000

    Actualización de backend.js desde Colab

---
+++ backend.js
@@ -0,0 +1,123 @@
+const appData = {
+"jugadores": [
+        {
+            "id_jugador": 1,
+            "nombre": "DAVID"
+        },
+        {
+            "id_jugador": 2,
+            "nombre": "MELERO"
+        },
+        {
+            "id_jugador": 3,
+            "nombre": "JESUS"
+        },
+        {
+            "id_jugador": 4,
+            "nombre": "NACHO"
+        },
+        {
+            "id_jugador": 5,
+            "nombre": "CHAMPI"
+        },
+        {
+            "id_jugador": 6,
+            "nombre": "JAVI"
+        },
+        {
+            "id_jugador": 7,
+            "nombre": "GONZALO"
+        },
+        {
+            "id_jugador": 8,
+            "nombre": "OSCAR"
+        }
+    ],
+    "partidos": [
+        {
+            "id_partido": 1,
+            "fecha": "2026-04-09"
+        },
+        {
+            "id_partido": 2,
+            "fecha": "2026-04-09"
+        }
+    ],
+    "parejas": [
+        {
+            "id_pareja": 1,
+            "id_jugador1": 1,
+            "id_jugador2": 2
+        },
+        {
+            "id_pareja": 2,
+            "id_jugador1": 3,
+            "id_jugador2": 4
+        },
+        {
+            "id_pareja": 3,
+            "id_jugador1": 5,
+            "id_jugador2": 6
+        },
+        {
+            "id_pareja": 4,
+            "id_jugador1": 7,
+            "id_jugador2": 8
+        }
+    ],
+    "resultados": [
+        {
+            "id_resultado": 1,
+            "id_partido": 1,
+            "equipo_ganador": 1
+        }
+    ],
+    "sets": [
+        {
+            "id_set": 1,
+            "id_partido": 1,
+            "numero_set": 1,
+            "juegos_equipo1": 4,
+            "juegos_equipo2": 6
+        },
+        {
+            "id_set": 2,
+            "id_partido": 1,
+            "numero_set": 2,
+            "juegos_equipo1": 6,
+            "juegos_equipo2": 2
+        },
+        {
+            "id_set": 3,
+            "id_partido": 1,
+            "numero_set": 3,
+            "juegos_equipo1": 7,
+            "juegos_equipo2": 5
+        }
+    ],
+    "partido_pareja": [
+        {
+            "id_partido_pareja": 1,
+            "id_partido": 1,
+            "id_pareja": 1,
+            "equipo": 1
+        },
+        {
+            "id_partido_pareja": 2,
+            "id_partido": 1,
+            "id_pareja": 2,
+            "equipo": 2
+        },
+        {
+            "id_partido_pareja": 3,
+            "id_partido": 2,
+            "id_pareja": 3,
+            "equipo": 1
+        },
+        {
+            "id_partido_pareja": 4,
+            "id_partido": 2,
+            "id_pareja": 4,
+            "equipo": 2
+        }
+    ]};
