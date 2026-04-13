6d782c38d463d0ae52b4194c0757fe5d6d311d63
PadelRivas <yayoscantera@gmail.com>
2026-04-13 10:54:20 +0000

    Actualización de backend.js desde Colab

--- backend.js
+++ backend.js
@@ -2,45 +2,37 @@ const appData = {
 "jugadores": [
         {
             "id_jugador": 1,
-            "nombre": "DAVID"
-        },
-        {
-            "id_jugador": 2,
-            "nombre": "MELERO"
-        },
-        {
-            "id_jugador": 3,
-            "nombre": "JESUS"
-        },
-        {
-            "id_jugador": 4,
-            "nombre": "NACHO"
-        },
-        {
-            "id_jugador": 5,
             "nombre": "CHAMPI"
         },
         {
+            "id_jugador": 2,
+            "nombre": "JESUS"
+        },
+        {
+            "id_jugador": 3,
+            "nombre": "NACHO"
+        },
+        {
+            "id_jugador": 4,
+            "nombre": "TONI"
+        },
+        {
+            "id_jugador": 5,
+            "nombre": "ALMUZARA"
+        },
+        {
             "id_jugador": 6,
-            "nombre": "JAVI"
-        },
-        {
-            "id_jugador": 7,
-            "nombre": "GONZALO"
-        },
-        {
-            "id_jugador": 8,
-            "nombre": "OSCAR"
+            "nombre": "DAVID"
         }
     ],
     "partidos": [
         {
             "id_partido": 1,
-            "fecha": "2026-04-09"
+            "fecha": "2026-04-16"
         },
         {
             "id_partido": 2,
-            "fecha": "2026-04-09"
+            "fecha": "2026-04-16"
         }
     ],
     "parejas": [
@@ -56,24 +48,19 @@ const appData = {
         },
         {
             "id_pareja": 3,
-            "id_jugador1": 5,
-            "id_jugador2": 6
+            "id_jugador1": 2,
+            "id_jugador2": 5
         },
         {
             "id_pareja": 4,
-            "id_jugador1": 7,
-            "id_jugador2": 8
+            "id_jugador1": 1,
+            "id_jugador2": 6
         }
     ],
     "resultados": [
         {
             "id_resultado": 1,
             "id_partido": 1,
-            "equipo_ganador": 1
-        },
-        {
-            "id_resultado": 2,
-            "id_partido": 2,
             "equipo_ganador": 1
         }
     ],
@@ -82,36 +69,15 @@ const appData = {
             "id_set": 1,
             "id_partido": 1,
             "numero_set": 1,
-            "juegos_equipo1": 4,
-            "juegos_equipo2": 6
+            "juegos_equipo1": 6,
+            "juegos_equipo2": 5
         },
         {
             "id_set": 2,
             "id_partido": 1,
             "numero_set": 2,
             "juegos_equipo1": 6,
-            "juegos_equipo2": 2
-        },
-        {
-            "id_set": 3,
-            "id_partido": 1,
-            "numero_set": 3,
-            "juegos_equipo1": 7,
-            "juegos_equipo2": 5
-        },
-        {
-            "id_set": 4,
-            "id_partido": 2,
-            "numero_set": 1,
-            "juegos_equipo1": 6,
-            "juegos_equipo2": 2
-        },
-        {
-            "id_set": 5,
-            "id_partido": 2,
-            "numero_set": 2,
-            "juegos_equipo1": 6,
-            "juegos_equipo2": 2
+            "juegos_equipo2": 0
         }
     ],
     "partido_pareja": [
